c8be57f711cc4d104420aa305dbd6f3ff276786b
miaolq08 <2607027903@qq.com>
2020-08-04 20:53:23 +0800

    Initial commit

---
+++ src/main/java/com/miao/entity/User.java
@@ -0,0 +1,34 @@
+package com.miao.entity;
+
+import com.miao.entity.Auth;
+
+import java.util.List;
+
+public class User {
+	
+	private String userType;
+	
+	private List<Auth> menuList;
+	private List<Auth> urlList;
+	
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	
+	public List<Auth> getMenuList() {
+		return menuList;
+	}
+	public void setMenuList(List<Auth> menuList) {
+		this.menuList = menuList;
+	}
+
+	public List<Auth> getUrlList() {
+		return urlList;
+	}
+	public void setUrlList(List<Auth> urlList) {
+		this.urlList = urlList;
+	}
+}
